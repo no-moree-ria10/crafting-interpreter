bd0310471652d00ea2c39166db2ab8740aeb4e94
ria10 <otatakuma0714@gmail.com>
2018-10-24 17:59:31 +0900

    first commit

---
+++ src/main.c
@@ -0,0 +1,38 @@
+#include "common.h"
+#include "chunk.h"
+#include "debug.h"
+#include "vm.h"
+
+int main(int argc, const char* argv[]){
+    initVM();
+    
+    Chunk chunk;
+    initChunk(&chunk);
+
+    int constant = addConstant(&chunk, 1.2);
+    writeChunk(&chunk,OP_CONSTANT, 123);
+    writeChunk(&chunk,constant, 123);
+
+    constant = addConstant(&chunk, 1.5);
+    writeChunk(&chunk,OP_CONSTANT, 123);
+    writeChunk(&chunk,constant, 123);
+    
+    writeChunk(&chunk, OP_RETURN,124);
+    //    writeChunk(&chunk, 0);
+
+    disassembleChunk(&chunk, "test chunk");
+    interpret(&chunk);
+    
+    freeVM();
+    freeChunk(&chunk);
+    
+    return 0;
+}
+
+
+
+
+
+
+    
+
