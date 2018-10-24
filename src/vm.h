9c42494de226a9b37645418233859c1704a45c3a
ria10 <otatakuma0714@gmail.com>
2018-10-24 18:34:42 +0900

    change suffix

--- src/vm.h
+++ src/vm.h
@@ -3,9 +3,14 @@
 
 #include "chunk.h"
 
+
+#define STACK_MAX 256
+
 typedef struct {  
-    Chunk* chunk;
+    Chunk*   chunk;
     uint8_t* ip;
+    Value    stack[STACK_MAX];
+    Value*   stackTop;
 } VM;             
 
 typedef enum{
